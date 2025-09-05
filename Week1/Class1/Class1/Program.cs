eb142a6ff96db43d92f6a42bd2abfd033d1cbd89
Navrose Johal <johnavro@sheridancollege.ca>
2025-09-05 15:24:26 -0400

    Initialized repo

---
+++ Week1/Class1/Class1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter circle radius");
+double r = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine( "area of circle " + (Math.PI * r*r)); // or Math.Pow(value, power)
+Console.WriteLine("circumference of circle " + (Math.PI * r * 2));
+Console.WriteLine("Diameter of circle " + (2 * r));
+
+// string interpolation - $"string {variable}"
+// concatenation makes program vulnerable
+// ( $"string {0}", area )
