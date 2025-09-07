9a2ec5c85e67b4944589123d293b4b01b9c9135b
Navrose Johal <johnavro@sheridancollege.ca>
2025-09-06 22:14:41 -0400

    Made a celcius to Farenheit convertor

---
+++ O'reilly Learning/O'reilly Learning/O'reilly Learning - 1/CelciusToFarenheit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O_reilly_Learning___1
+{
+    public class CelciusToFarenheit
+    {
+        public static void run()
+        {
+            Console.WriteLine("Enter the temp in Celcius");
+            string input = Console.ReadLine()!;
+            bool success = double.TryParse(input, out double celcius);
+
+            if (success)
+            {
+                Console.WriteLine($"{celcius} C = " + ((celcius * 9 / 5) + 32) + " Farenheit");
+            }
+        }
+    }
+}
---
+++ O'reilly Learning/O'reilly Learning/O'reilly Learning - 1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ 
+Normally, value types (like int) cannot store null.
+But you can make them nullable with ?.
+
+int? optionalNumber = null;
+
+*/
+
+// we also have var, dynamic and const 
+
+using O_reilly_Learning___1;
+
+//var x = 0;
+//dynamic z = 0;
+//z = "hel;p";
+//const double PI  = Math.PI;
+
+// Conversions 
+//int a = 10;
+
+//double b = a; // Implicit
+
+double x1 = 9.8;
+int y = (int)x1; // Explicit
+Console.WriteLine(y);
+
+
+string line = "123";
+int number = int.Parse(line); // throws error if invalid
+
+Console.WriteLine(number);
+bool success = int.TryParse(line, out int safeNum); // safer way to doing string to int conversion
+
+// if fails then value of safeNum will be 0
+if(success){
+Console.WriteLine(safeNum);
+}
+
+CelciusToFarenheit.run();
