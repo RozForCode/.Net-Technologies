cb542b997e58d86d7368771c832af577ab95963c
Navrose Johal <johnavro@sheridancollege.ca>
2025-09-10 10:06:04 -0400

    Nullable data types, type inferance, conditional and loop statements

---
+++ Week1/Class1/Class2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter sub total");
+
+double subTotal = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Enter Discount Percentage:");
+
+double discountPercent = Convert.ToDouble(Console.ReadLine()!);
+
+double discountAmount = subTotal * (discountPercent / 100);
+double total = subTotal - discountAmount;
+
+Console.WriteLine($"Sub Total: {subTotal:C2}");
+Console.WriteLine($"Discount Percent: {discountPercent}%");
+Console.WriteLine($"Discount Amount: {discountAmount:C2}");
