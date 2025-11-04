225c8baedf505f31e6f5ad9807df63bee8ab9b24
Navrose Johal <johnavro@sheridancollege.ca>
2025-11-04 11:02:05 -0500

    wpf apps

---
+++ WPF apps/WPF apps/LoginWindow/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public string getString(TextBox textBox)
+        {
+            return textBox.Text;
+        }
+        public void OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        public void OnClickAdd(object sender, RoutedEventArgs e)
+        {
+            string userName = "Nav";
+            string password = "Van";
+
+            if(userName.Equals(getString(Nav)) && password.Equals(getString(Van)))
+            {
+            Window win1 = new Window();
+            win1.Background = Brushes.Cyan; win1.Title = "Window 1";
+            win1.Height = 250; win1.Width = 350; win1.Content = "Independent Window";
+            win1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login","Error",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ WPF apps/WPF apps/wpf app/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise_Week5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private double GetNumber(TextBox txt)
+        {
+            double num;
+            if (!double.TryParse(txt.Text, out num))
+            {
+                MessageBox.Show("Please enter valid numbers.");
+                throw new InvalidOperationException();
+            }
+            return num;
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double n1 = GetNumber(txtNumber1);
+                double n2 = GetNumber(txtNumber2);
+                lblResult.Content = $"Sum = {n1 + n2}";
+            }
+            catch { }
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double n1 = GetNumber(txtNumber1);
+                double n2 = GetNumber(txtNumber2);
+                lblResult.Content = $"Difference = {n1 - n2}";
+            }
+            catch { }
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double n1 = GetNumber(txtNumber1);
+                double n2 = GetNumber(txtNumber2);
+                lblResult.Content = $"Product = {n1 * n2}";
+            }
+            catch { }
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double n1 = GetNumber(txtNumber1);
+                double n2 = GetNumber(txtNumber2);
+                if (n2 == 0)
+                {
+                    MessageBox.Show("Cannot divide by zero!");
+                    return;
+                }
+                lblResult.Content = $"Quotient = {n1 / n2}";
+            }
+            catch { }
+
+        }
+    }
+}
