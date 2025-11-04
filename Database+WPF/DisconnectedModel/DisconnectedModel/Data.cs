acb6527bb625eb5ddd3491105e05f7b26195756a
Navrose Johal <johnavro@sheridancollege.ca>
2025-11-04 16:42:12 -0500

    some button functionality tied to db

---
+++ Database+WPF/DisconnectedModel/DisconnectedModel/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+
+namespace DisconnectedModel
+{
+    public class Data
+    {
+        private static string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;
+Initial Catalog=Northwind;
+Integrated Security=True";
+        public static string ConnectionString { get => connStr; }
+        public DataTable GetAllProducts()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            string query = "Select ProductID, ProductName, UnitPrice, UnitsInStock from Products";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds,"Products");
+            DataTable tblProdcuts = ds.Tables["Products"];
+            return tblProdcuts;
+        }
+    }
+
+}
---
+++ Database+WPF/DisconnectedModel/DisconnectedModel/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisconnectedModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Data data = new Data();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            grdProducts.ItemsSource = data.GetAllProducts().DefaultView;
+        }
+    }
+}
